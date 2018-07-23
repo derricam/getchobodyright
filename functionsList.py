d05a2f6a23c19882e014e2eaa69154e11fa63600
Doriandovai <40011769+Doriandovai@users.noreply.github.com>
2018-07-23 16:09:33 -0500

    456

--- functionsList.py
+++ functionsList.py
@@ -4,157 +4,76 @@ def startAvatarPage():
     
     textSize(20)
     text("LEFT MOUSE = BODY", 200, 700)
-    text("CENTER MOUSE = ", 200, 725)
-    text("LEFT MOUSE = BODY", 200, 750)
+    text("CENTER MOUSE = SHOES ", 200, 725)
+    text("RIGHT MOUSE = HAT", 200, 750)
     
     
     
     
     
-    
+    #1
     fill(255,255,255)
     rect(0,0,50,50)
-    
+    #2
     fill(0,0,0)
     rect(0,50,50,50)
-    
-    fill(247, 2, 39)
+    #3
+    fill(255, 0, 0)
     rect(0,100,50,50)
-    
-    fill(216, 2, 138)
+    #4
+    fill(0, 255, 0)
     rect(0,150,50,50)
-    
-    fill(144, 2, 216)
+    #5
+    fill(0, 0, 255)
     rect(0,200,50,50)
-    
-    fill(150, 60, 216)
+    #6
+    fill(255, 255, 0)
     rect(0,250,50,50)
-    
-    fill(100, 216, 215)
+    #7
+    fill(0, 255, 255)
     rect(0,300,50,50)
     
-    fill(2, 216, 111)
-    rect(50,0,50,50)
     
-    fill(37, 216, 2)
-    rect(50,50,50,50)
-    
-    fill(247, 247, 5)
-    rect(50,100,50,50)
-    
-    fill(204, 205, 30)
-    rect(50,150,50,50)
-    
-    fill(252, 188, 60)
-    rect(50,200,50,50)
-    
-    fill(250, 133, 8)
-    rect(50,250,50,50)
-    
-    fill(90, 77, 72)
-    rect(50,300,50,50)
 
 def drawBody():
         if mousePressed and mouseButton == LEFT:
             if mouseX <= 150 and mouseY <= 400:
-                print("1st")
+                ellipse(350, 50, 50, 75)
         if mousePressed and mouseButton == LEFT:
             if mouseX <= 150 and mouseY <= 400:
-                print("2nd")
+                ellipse(350, 200, 125, 200)
         
-def mouseFunction():
-    
-        if mouseButton == LEFT:
-
-            if mouseX <= 50 and mouseY < 50:
-                fill(255, 255, 255)
-            if mouseX <= 50 and mouseY > 50 and mouseY <=100:
-                fill(0, 0, 0)
-            if mouseX <= 50 and mouseY > 100 and mouseY <=150:
-                fill(247, 2, 39)
-            if mouseX <=50 and mouseY > 150 and mouseY <=200:
-                fill(216, 2, 138)
-            if mouseX <=50 and mouseY > 200 and mouseY <=250:
-                fill(144, 2, 216)
-            if mouseX <=50 and mouseY > 250 and mouseY <=300:
-                fill(150, 60, 216)
-            if mouseX <=50 and mouseY > 300 and mouseY <=350:
-                fill(100, 216, 215)
-            if mouseX > 50 and mouseY < 100 and mouseY <=50:
-                fill(2, 216, 111)
-            if mouseX > 50 and mouseY < 150 and mouseY <=100:
-                fill(37, 216, 2)
-            if mouseX > 50 and mouseY < 200 and mouseY <=150:
-                fill(247, 247, 5)
-            if mouseX > 50 and mouseY < 250 and mouseY <=200:
-                fill(204, 205, 30)
-            if mouseX > 50 and mouseY < 300 and mouseY <=250:
-                fill(252, 188, 60)
-            if mouseX > 50 and mouseY < 350 and mouseY <=300:
-                fill(250, 133, 8)
-            if mouseX > 50 and mouseY < 400 and mouseY <=350:
-                fill(90, 77, 72)
-        ellipse(350, 50, 50, 75)
-        ellipse(350, 200, 125, 200) 
+def mouseFunction():         
         if mouseButton == RIGHT:
             if mouseX <= 50 and mouseY < 50:
                 fill(255, 255, 255)
             if mouseX <= 50 and mouseY > 50 and mouseY <=100:
                 fill(0, 0, 0)
             if mouseX <= 50 and mouseY > 100 and mouseY <=150:
-                fill(247, 2, 39)
+                fill(255, 0, 0)
             if mouseX <=50 and mouseY > 150 and mouseY <=200:
-                fill(216, 2, 138)
+                fill(0, 255, 0)
             if mouseX <=50 and mouseY > 200 and mouseY <=250:
-                fill(144, 2, 216)
+                fill(0, 0, 255)
             if mouseX <=50 and mouseY > 250 and mouseY <=300:
-                fill(150, 60, 216)
+                fill(255, 255, 0)
             if mouseX <=50 and mouseY > 300 and mouseY <=350:
-                fill(100, 216, 215)
-            if mouseX > 50 and mouseY < 100 and mouseY <=50:
-                fill(2, 216, 111)
-            if mouseX > 50 and mouseY < 150 and mouseY <=100:
-                fill(37, 216, 2)
-            if mouseX > 50 and mouseY < 200 and mouseY <=150:
-                fill(247, 247, 5)
-            if mouseX > 50 and mouseY < 250 and mouseY <=200:
-                fill(204, 205, 30)
-            if mouseX > 50 and mouseY < 300 and mouseY <=250:
-                fill(252, 188, 60)
-            if mouseX > 50 and mouseY < 350 and mouseY <=300:
-                fill(250, 133, 8)
-            if mouseX > 50 and mouseY < 400 and mouseY <=350:
-                fill(90, 77, 72)
+                fill(0, 255, 255)
         triangle(350, 0, 400, 45, 300, 25)
     
         if mouseButton == CENTER:
-        noStroke()
-        if mouseX <= 50 and mouseY < 50:
-            fill(255, 255, 255)
-        if mouseX <= 50 and mouseY > 50 and mouseY <=100:
-            fill(0, 0, 0)
-        if mouseX <= 50 and mouseY > 100 and mouseY <=150:
-            fill(247, 2, 39)
-        if mouseX <=50 and mouseY > 150 and mouseY <=200:
-            fill(216, 2, 138)
-        if mouseX <=50 and mouseY > 200 and mouseY <=250:
-            fill(144, 2, 216)
-        if mouseX <=50 and mouseY > 250 and mouseY <=300:
-            fill(150, 60, 216)
-        if mouseX <=50 and mouseY > 300 and mouseY <=350:
-            fill(100, 216, 215)
-        if mouseX > 50 and mouseY < 100 and mouseY <=50:
-            fill(2, 216, 111)
-        if mouseX > 50 and mouseY < 150 and mouseY <=100:
-            fill(37, 216, 2)
-        if mouseX > 50 and mouseY < 200 and mouseY <=150:
-            fill(247, 247, 5)
-        if mouseX > 50 and mouseY < 250 and mouseY <=200:
-            fill(204, 205, 30)
-        if mouseX > 50 and mouseY < 300 and mouseY <=250:
-            fill(252, 188, 60)
-        if mouseX > 50 and mouseY < 350 and mouseY <=300:
-            fill(250, 133, 8)
-        if mouseX > 50 and mouseY < 400 and mouseY <=350:
-            fill(90, 77, 72)
+            if mouseX <= 50 and mouseY < 50:
+                fill(255, 255, 255)
+            if mouseX <= 50 and mouseY > 50 and mouseY <=100:
+                fill(0, 0, 0)
+            if mouseX <= 50 and mouseY > 100 and mouseY <=150:
+                fill(255, 0, 0)
+            if mouseX <=50 and mouseY > 150 and mouseY <=200:
+                fill(0, 255, 0)
+            if mouseX <=50 and mouseY > 200 and mouseY <=250:
+                fill(0, 0, 255)
+            if mouseX <=50 and mouseY > 250 and mouseY <=300:
+                fill(255, 255, 0)
+            if mouseX <=50 and mouseY > 300 and mouseY <=350:
+                fill(0, 255, 255)
         triangle(350, 310, 400, 350, 300, 340)
